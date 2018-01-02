d2c191ef7b7c20edb5a3a561ca84831684e048ce
Matthew Ha <matthewha123@gmail.com>
2018-01-02 14:13:00 -0500

    doc strings

--- ColorChange.py
+++ ColorChange.py
@@ -36,22 +36,33 @@ def removeColor(img):
     img.putdata(pxList)
     return img
 def chooseColor():
+    '''
+    Uses Kevin Wuhoo's python port of David Merfield's random color picker
+    Modified randomcolor to return list of ints instead of string
+    '''
     chooser = randomcolor.RandomColor()
     lum = ['random']
     color = chooser.generate(hue = 'random', luminosity = random.choice(lum),format_ = 'rgb')
     print(color)
     return color
+
 im = removeColor('suckFacev2.png')
 im.save('wall.png')
+im.show()
+
+#if windows
 if(os.name == 'nt'):
     ctypes.windll.user32.SystemParametersInfoW(20, 0, "C:/Users/matth/.spyder-py3/suckFace/wall.png" , 0)
+    
+#if mac
 elif(os.name == 'mac'):
+    #need to put in file path where it is specified in script
    SCRIPT = """/usr/bin/osascript<<END
    tell application "Finder"
-   set desktop picture to POSIX file "%s"
+   set desktop picture to POSIX file "FILEPATH_TO_WALL.PNG"
    end tell
    END"""
    subprocess.Popen(SCRIPT%filename, shell=True)
-im.show()
+   
 
     
